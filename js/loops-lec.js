83710b9cd4d1e31589ca0e2359a659346e30f38e
Amber Mattis <amber.mattis2@gmail.com>
2020-10-02 14:46:36 -0500

    for loops mostly completed

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -1,126 +1,135 @@
-"use strict";
-/*********************************************
- *                   WHILE LOOPS
- *********************************************/
-// A while loop will run WHILE a certain condition is true
-// while (condition) {
-//  code runs.
-// }
-
-//TODO TOGETHER: Create a while loop that counts up to 10 (non-inclusive) by 1
-// (0, 1, 2...9)
-
-var i = 0;
-while(i < 10) {
-    console.log(i)
-    i++;
-}
-
-
-
-//TODO TOGETHER: Create a while loop that counts down from 20 to 0 (inclusive) by 1
-// (20, 19, 18...0)
-
-var i = 20
-while(i >= 0){
-    console.log(i)
-    i--;
-}
-
-//TODO TOGETHER: Create a while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
-// (0, 2, 4...20)
-
-var i = 0
-while(i <= 20){
-    console.log(i)
-    i+=2; //addition and reassignment
-}
-
-//TODO: Create a while loop that counts to 100 (inclusive) in increments of ten:
-// (10...20...30...)
-
-var i = 10
-while(i <= 100){
-    console.log(i)
-    i+=10;
-}
-
-//TODO: Create a while loop that counts down from 50 to 1 (inclusive) in increments of 1:
-// (50, 49, 48, 47, 46 .... 1)
-
-var i = 50
-while(i >= 1){
-    console.log(i)
-    i--;
-}
-
-/*********************************************
- *               DO-WHILE LOOPS
- *********************************************/
-// A do/while loop will run at least once.
-
-// do {
-//  Code to be run.
-// } while (condition)
-
-// var i = 10;
+// "use strict";
+// /*********************************************
+//  *                   WHILE LOOPS
+//  *********************************************/
+// // A while loop will run WHILE a certain condition is true
+// // while (condition) {
+// //  code runs.
+// // }
 //
-// while (i < 10) {
-// 	console.log('while loop iteration #' + i);
-// 	i++;
+// //TODO TOGETHER: Create a while loop that counts up to 10 (non-inclusive) by 1
+// // (0, 1, 2...9)
+//
+// var i = 0;
+// while(i < 10) {
+//     console.log(i)
+//     i++;
 // }
-
-// TODO TOGETHER: Why doesnt the example above run?
-    //
-// TODO TOGETHER: Refactor the example above using a do-while loop. What behavior do we notice?
-
-var i = 10;
-    do {
-        console.log("while loop iteration #" + i);
-        i++;
-    } while(i < 10);
-
-//TODO TOGETHER: Create a do-while loop that asks the user to confirm: "Would you like to exit?" What difference do you notice?
-
-var toExit;
-    do{
-       toExit = confirm("Would you like to exit?");
-    } while(!toExit);
-
-
-//TODO TOGETHER: Create a do-while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
-// (0, 2, 4...20)
-
-var i = 0;
-
-    do{
-        console.log(i)
-        i+=2;
-    } while(i <= 20)
-
-//TODO: Create a do-while loop that prompts the user for a color and continues to prompt until the string passed is "blue"
-
-   do{
-     var input = prompt("Enter color");
-   } while(input !== "blue")
-    alert("Blue is my favorite color too!")
-/*****************************************
- *               FOR LOOPS
- *********************************************/
-// For loops: Loops that are designed to run a given number of times.
-
-// for (/*initialization*/; /*condition*/; /*increment*/) {
-// 	// body
+//
+//
+//
+// //TODO TOGETHER: Create a while loop that counts down from 20 to 0 (inclusive) by 1
+// // (20, 19, 18...0)
+//
+// var i = 20
+// while(i >= 0){
+//     console.log(i)
+//     i--;
 // }
+//
+// //TODO TOGETHER: Create a while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
+// // (0, 2, 4...20)
+//
+// var i = 0
+// while(i <= 20){
+//     console.log(i)
+//     i+=2; //addition and reassignment
+// }
+//
+// //TODO: Create a while loop that counts to 100 (inclusive) in increments of ten:
+// // (10...20...30...)
+//
+// var i = 10
+// while(i <= 100){
+//     console.log(i)
+//     i+=10;
+// }
+//
+// //TODO: Create a while loop that counts down from 50 to 1 (inclusive) in increments of 1:
+// // (50, 49, 48, 47, 46 .... 1)
+//
+// var i = 50
+// while(i >= 1){
+//     console.log(i)
+//     i--;
+// }
+//
+// /*********************************************
+//  *               DO-WHILE LOOPS
+//  *********************************************/
+// // A do/while loop will run at least once.
+//
+// // do {
+// //  Code to be run.
+// // } while (condition)
+//
+// // var i = 10;
+// //
+// // while (i < 10) {
+// // 	console.log('while loop iteration #' + i);
+// // 	i++;
+// // }
+//
+// // TODO TOGETHER: Why doesnt the example above run?
+//     //
+// // TODO TOGETHER: Refactor the example above using a do-while loop. What behavior do we notice?
+//
+// var i = 10;
+//     do {
+//         console.log("while loop iteration #" + i);
+//         i++;
+//     } while(i < 10);
+//
+// //TODO TOGETHER: Create a do-while loop that asks the user to confirm: "Would you like to exit?" What difference do you notice?
+//
+// var toExit;
+//     do{
+//        toExit = confirm("Would you like to exit?");
+//     } while(!toExit);
+//
+//
+// //TODO TOGETHER: Create a do-while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
+// // (0, 2, 4...20)
+//
+// var i = 0;
+//
+//     do{
+//         console.log(i)
+//         i+=2;
+//     } while(i <= 20)
+//
+// //TODO: Create a do-while loop that prompts the user for a color and continues to prompt until the string passed is "blue"
+//
+//    do{
+//      var input = prompt("Enter color");
+//    } while(input !== "blue")
+//     alert("Blue is my favorite color too!")
+// /*****************************************
+//  *               FOR LOOPS
+//  *********************************************/
+// // For loops: Loops that are designed to run a given number of times.
+//
+// // for (/*initialization*/; /*condition*/; /*increment*/) {
+// // 	// body
+// // }
+//
+// //TODO TOGETHER: Explore basic for loop syntax
+//
+//
+// // TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
+//     for(i = 0; i <= 5; i++){
+//         console.log(i);
+//     }
+// // TODO TOGETHER: Why are loops beneficial?
+//     //Because it prevents redundancies
+//     // iteration: going over each item in a list
+// // TODO TOGETHER: Console.log each color from the array randomColors
+//    var randomColors = ["red","orange","yellow","blue","green"]
+//
+//     for(var i = 0; i < randomColors.length; i++){
+//         console.log(randomColors[i]);
+//     }
 
-//TODO TOGETHER: Explore basic for loop syntax
-
-// TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
-
-// TODO TOGETHER: Why are loops beneficial?
-
-// TODO TOGETHER: Console.log each color from the array randomColors
-//  var randomColors = ["red","orange","yellow","blue","green"]
 
 // for (var i = 0; i < randomColors.length; i++){
 // 	console.log(randomColors[i]);;
@@ -136,6 +145,17 @@ var i = 0;
 //  For numbers divisible by 5, log "Buzz"
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
+for(i = 0; i < 101; i++){
+   else if(i % 3 ===0) {
+        console.log("Fizz")
+    }else if(i % 5 === 0){
+        console.log("Buzz")
+    }else if(i % 3 === 0 && i % 5 === 0 ) {
+        console.log("FizzBuzz")
+    }else{
+        console.log(i)
+    }
+}
 
 /*********************************************
  *              BREAKS & CONTINUES
