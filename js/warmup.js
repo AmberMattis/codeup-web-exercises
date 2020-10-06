b92942fcbb5454d0a38e303cd85655d338c9f0a6
Amber Mattis <amber.mattis2@gmail.com>
2020-10-06 12:06:27 -0500

    updated planets-string exercise

--- js/warmup.js
+++ js/warmup.js
@@ -1,17 +1,47 @@
-var name1 = "Amber";
-var name2 = "Ashley";
-var name3 = "Andrea";
-var name4 = "Ariel";
-var name5 = "Ash";
+// var name1 = "Amber";
+// var name2 = "Ashley";
+// var name3 = "Andrea";
+// var name4 = "Ariel";
+// var name5 = "Ash";
+//
+// console.log("Hello my name is " + name1);
+// console.log("Hello my name is " + name2);
+// console.log("Hello my name is " + name3);
+// console.log("Hello my name is " + name4);
+// console.log("Hello my name is " + name5);
+//
+// function sayHello(name) {
+//     console.log(":Hello" + name);
+// }
+// sayHello("Amber");
+// // test
 
-console.log("Hello my name is " + name1);
-console.log("Hello my name is " + name2);
-console.log("Hello my name is " + name3);
-console.log("Hello my name is " + name4);
-console.log("Hello my name is " + name5);
+// debugging functions
 
-function sayHello(name) {
-    console.log(":Hello" + name);
-}
-sayHello("Amber");
-// test
+
+// function sum3s(numbers) {
+//     var output;
+//     numbers.forEach(function(number) {
+//         if (number === 3 ) {
+//             output += 3;
+//         }
+//     });
+//     return output;
+// }
+//
+// console.log(sum3s(['a', 3, '3', 3, null]));
+
+
+
+var pies = ["apple", "cherry", "key lime", "huckleberry", "rhubarb", "key lime"];
+
+//
+// function recentPies(lastThree){
+//     var lastThree = pies.slice(3);
+//     return lastPies;
+// }
+
+function recentPies(last){
+    var lastThree = pies.length -3;
+    return lastThree
+})
