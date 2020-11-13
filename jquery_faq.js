629768a58ff2ed18b4a99d1cb4c3dd1e0f988fe1
Amber Mattis <amber.mattis2@gmail.com>
2020-11-13 13:01:50 -0600

    jquery faq

---
+++ jquery_faq.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+
+
+    $("#a-link").click(function (e) {
+        e.preventDefault();
+        $("#a-link").toggleClass("highlight")
+        $("#a-def").toggleClass("invisible")
+    });
+
+    $("#b-link").click(function (e) {
+        e.preventDefault();
+        $("#b-link").toggleClass("highlight")
+        $("#b-def").toggleClass("invisible")
+    });
+
+    $("#c-link").click(function (e) {
+        e.preventDefault();
+        $("#c-link").toggleClass("highlight")
+        $("#c-def").toggleClass("invisible")
+    });
+
+    $("#d-link").click(function (e) {
+        e.preventDefault();
+        $("#d-link").toggleClass("highlight")
+        $("#d-def").toggleClass("invisible")
+    });
+
+    $("#e-link").click(function (e) {
+        e.preventDefault();
+        $("#e-link").toggleClass("highlight")
+        $("#e-def").toggleClass("invisible")
+    });
+
+    $("#f-link").click(function (e) {
+        e.preventDefault();
+        $("#f-link").toggleClass("highlight")
+        $("#f-def").toggleClass("invisible")
+    });
+
+    $("#g-link").click(function (e) {
+        e.preventDefault();
+        $("#g-link").toggleClass("highlight")
+        $("#g-def").toggleClass("invisible")
+    });
+
+    $("#h-link").click(function (e) {
+        e.preventDefault();
+        $("#h-link").toggleClass("highlight")
+        $("#h-def").toggleClass("invisible")
+    });
+
+
+    $("#i-link").click(function (e) {
+        e.preventDefault();
+        $("#i-link").toggleClass("highlight")
+        $("#i-def").toggleClass("invisible")
+    });
+
+    $("#j-link").click(function (e) {
+        e.preventDefault();
+        $("#j-link").toggleClass("highlight")
+        $("#j-def").toggleClass("invisible")
+    });
+
+});
