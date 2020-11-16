7abe349da337553f4d9725bcafc17aafd0e6f4e2
Amber Mattis <amber.mattis2@gmail.com>
2020-11-15 18:20:23 -0600

    added styling

--- jquery_faq.js
+++ jquery_faq.js
@@ -1,91 +1,85 @@
 $(document).ready(function() {
 
 
+     $("dd").hide();
+    //  $('dt').each(function() {
+    //     $(this).click(function(e) {
+    //         e.preventDefault();
+    //          $("dt").toggleClass("highlight")
+    //          $("dd").toggleClass("invisible").slideToggle();
+    //     });
+    //  });
 
 
-    $(window).on('load', function(){
-        $('#exampleModal').modal('show');
+    $("#a-link").click(function (e) {
+        e.preventDefault();
+        $("#a-link").toggleClass("highlight")
+        $("#a-def").toggleClass("invisible").slideToggle()
     });
 
-    $("dd").hide();
-     $('dt').each(function() {
-        $(this).click(function(e) {
-            e.preventDefault();
-             $("dt").toggleClass("highlight");
-             $("dd").toggleClass("invisible").slideToggle();
-        });
-     });
+    $("#b-link").click(function (e) {
+        e.preventDefault();
+        $("#b-link").toggleClass("highlight")
+        $("#b-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#c-link").click(function (e) {
+        e.preventDefault();
+        $("#c-link").toggleClass("highlight")
+        $("#c-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#d-link").click(function (e) {
+        e.preventDefault();
+        $("#d-link").toggleClass("highlight")
+        $("#d-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#e-link").click(function (e) {
+        e.preventDefault();
+        $("#e-link").toggleClass("highlight")
+        $("#e-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#f-link").click(function (e) {
+        e.preventDefault();
+        $("#f-link").toggleClass("highlight")
+        $("#f-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#g-link").click(function (e) {
+        e.preventDefault();
+        $("#g-link").toggleClass("highlight")
+        $("#g-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#h-link").click(function (e) {
+        e.preventDefault();
+        $("#h-link").toggleClass("highlight")
+        $("#h-def").toggleClass("invisible").slideToggle()
+    });
 
 
-    // $("#a-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#a-link").toggleClass("highlight")
-    //     $("#a-def").toggleClass("invisible")
-    // });
-    //
-    // $("#b-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#b-link").toggleClass("highlight")
-    //     $("#b-def").toggleClass("invisible")
-    // });
-    //
-    // $("#c-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#c-link").toggleClass("highlight")
-    //     $("#c-def").toggleClass("invisible")
-    // });
-    //
-    // $("#d-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#d-link").toggleClass("highlight")
-    //     $("#d-def").toggleClass("invisible")
-    // });
-    //
-    // $("#e-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#e-link").toggleClass("highlight")
-    //     $("#e-def").toggleClass("invisible")
-    // });
-    //
-    // $("#f-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#f-link").toggleClass("highlight")
-    //     $("#f-def").toggleClass("invisible")
-    // });
-    //
-    // $("#g-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#g-link").toggleClass("highlight")
-    //     $("#g-def").toggleClass("invisible")
-    // });
-    //
-    // $("#h-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#h-link").toggleClass("highlight")
-    //     $("#h-def").toggleClass("invisible")
-    // });
-    //
-    //
-    // $("#i-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#i-link").toggleClass("highlight")
-    //     $("#i-def").toggleClass("invisible")
-    // });
-    //
-    // $("#j-link").click(function (e) {
-    //     e.preventDefault();
-    //     $("#j-link").toggleClass("highlight")
-    //     $("#j-def").toggleClass("invisible")
-    // });
-    //
+    $("#i-link").click(function (e) {
+        e.preventDefault();
+        $("#i-link").toggleClass("highlight")
+        $("#i-def").toggleClass("invisible").slideToggle()
+    });
+
+    $("#j-link").click(function (e) {
+        e.preventDefault();
+        $("#j-link").toggleClass("highlight")
+        $("#j-def").toggleClass("invisible").slideToggle()
+    });
+
 
 
 
 
     $("button").click(function(e){
-        $("#messier-81-galaxy").children().last().css("background-color","yellow")
-        $("#centaurus-a-galaxy").children().last().css("background-color","yellow")
-        $("#messier-82-galaxy").children().last().css("background-color","yellow")
+        $("#messier-81-galaxy").children().last().css("background-color","navy")
+        $("#centaurus-a-galaxy").children().last().css("background-color","navy")
+        $("#messier-82-galaxy").children().last().css("background-color","navy")
 
     })
 
@@ -104,7 +98,7 @@ $(document).ready(function() {
 
     $("#galaxy1-heading").click(function(e){
         e.preventDefault();
-        $("#messier-81-galaxy").css("visibility","visible").children().slideToggle();
+        $("#messier-81-galaxy").removeClass("invisible").children().slideToggle();
 
     })
 
@@ -118,8 +112,8 @@ $(document).ready(function() {
 
     })
 
-    $("#state-parks-texas").click(function(){
-        $("#te")
-    })
 
+
+
+        $('#exampleModal').modal('show');
 });
