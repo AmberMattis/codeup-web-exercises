51e469bfe46c9f57ea809b62ed74be5153ce2fa9
Amber Mattis <amber.mattis2@gmail.com>
2020-11-16 21:31:02 -0600

    added blog data

---
+++ js/ajax-blog.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    "use strict"
+
+    function getBlogs(data) {
+        var blogInfo = $.ajax("data/blog.json")
+
+        blogInfo.done(function (data, status, jqXhr) {
+            console.log(data);
+            console.log(status);
+            console.log(jqXhr);
+            addHtml(data);
+        });
+
+        blogInfo.fail(function (data, error, status) {
+            console.log(error);
+            console.log(status);
+        });
+    };
+
+    function addHtml(inputs) {
+        var newHtml = "";
+
+        inputs.forEach(function(input){
+            newHtml += "<div class='first'>";
+            newHtml += "<h3>" + input.title + "</h3>";
+            newHtml += "<p>"  + input.content + "</p>";
+            newHtml += "<p>" + input.categories + "</p>";
+            newHtml += "<h4>" + input.date + "</h4>";
+            newHtml += "</div>";
+
+        })
+
+      $("#posts").html(newHtml);
+    }
+
+    $(window).load(getBlogs);
+})
