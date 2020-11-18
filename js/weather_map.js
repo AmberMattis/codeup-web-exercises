d734a864d4b302726599daf0ce67b297322721ac
Amber Mattis <amber.mattis2@gmail.com>
2020-11-17 22:01:25 -0600

    added weather map api

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,19 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function() {
     "use strict"
 
+    function getTodaysWeather() {
+       var requestWeather = $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: openWeather,
+            q: "San Antonio, US",
+           json
+        });
+           requestWeather.done(function (data) {
+            console.log("weather", data);
+            renderWeather(data);
+        })
+    }
 
-//     var sanWeather = $.get("http://api.openweathermap.org/data/2.5/weather?APPID=" + openWeather + "&q=San+Antonio,+US");
-//
-//     sanWeather.done(function(data,status,jqXhr){
-//         console.log(data);
-//         console.log(status);
-//     })
- })
+    function renderWeather(datas) {
+        var weatherHTML = "";
 
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: openWeather,
-    q: "San Antonio, US",
-    units: "imperial"
-}).done(function(data) {
-    console.log('5 day forecast', data);
+         for(const data of Object.keys(datas)){
+            weatherHTML += "<div class=\"card\" style=\"width: 18rem;\">";
+            weatherHTML += "<img class='card-img-top'>";
+            weatherHTML += "<div class='card-body'>";
+            weatherHTML += "<h5 class=\"card-title\">Current Weather</h5>";
+            weatherHTML += "<p class=\"card-text\"> + data.name + </p>";
+            weatherHTML += "<p class=\"card-text\"> + data.main.temp + </p>";
+            weatherHTML += "<p class=\"card-text\"> + weather[0].description + </p>";
+            weatherHTML += "</div>";
+            weatherHTML += "</div>";
+
+        };
+        $("#week").html(weatherHTML)
+    }
+
+    $(window).load(getTodaysWeather());
 });
+
+
+
+
